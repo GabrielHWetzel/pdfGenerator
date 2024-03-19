4ff3d1babd7a95b1b1d30f7dcf4efd4bcffd76ee
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-03-19 10:00:45 -0300

    Page generation based on csv page value

--- main.py
+++ main.py
@@ -2,15 +2,28 @@ from fpdf import FPDF
 import pandas
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
 df = pandas.read_csv("topics.csv")
 
-
 for index, row in df.iterrows():
-
     pdf.add_page()
 
-    pdf.set_font(family="Helvetica", style="B", size=16)
+    # Tittle
+    pdf.set_font(family="Helvetica", style="B", size=20)
+    pdf.set_text_color(60, 60, 60)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="C", ln=1)
     pdf.line(10, 23, 200, 23)
+
+    # Footer
+    pdf.ln(265)
+    pdf.set_font(family="Helvetica", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+    for i in range(row["Pages"]-1):
+        pdf.add_page()
+        pdf.ln(277)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output("output.pdf")
